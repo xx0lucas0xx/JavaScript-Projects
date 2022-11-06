e06910ed06c61b61ca6586f60e7d604a7c1fb39d
xx0lucas0xx <lucas.Rempart@gmail.com>
2022-11-05 19:07:46 -0600

    Test run
    testing out JavaScript

---
+++ Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,18 @@
+alert("Hii")
+window.alert("Hello")
+
+var A="Here's This is the beginning of the string" + "and this is the end of the string"
+
+
+document.write(A)
+
+var B="Here is \"another\"string"
+
+document.write(B)
+
+document.write("Here is some text" + "Here is some more text")
+
+//family
+var Family="The Cunninghams", Dad="Adam", Daughter="Keana", Son="Charlie";
+document.write(Dad)
+
