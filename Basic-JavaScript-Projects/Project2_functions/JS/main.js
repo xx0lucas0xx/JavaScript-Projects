b41aa1fb88ff0ff196ba87786cff0f2f71282ddb
xx0lucas0xx <lucas.Rempart@gmail.com>
2022-11-07 08:13:01 -0700

    Cleaned
    cleaned up the code to submit for assignment

---
+++ Basic-JavaScript-Projects/Project2_functions/JS/main.js
@@ -0,0 +1,7 @@
+
+//for 'click me!'
+function myfunction()  {
+    var sentence = "I like to eat bagels";
+    sentence += "at 3:00 am"
+    document.getElementById("concantenate").innerHTML = sentence;
+}
