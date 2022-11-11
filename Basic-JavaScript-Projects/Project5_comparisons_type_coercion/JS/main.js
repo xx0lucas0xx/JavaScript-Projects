eb0e94ba04dc058c003b57720eae9e18ff7db61a
xx0lucas0xx <lucas.Rempart@gmail.com>
2022-11-11 09:38:00 -0700

    Operator cleanup
    cleaned up the operators main.js and index.html

--- Basic-JavaScript-Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic-JavaScript-Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,2 +1,28 @@
-document.write(typeof "Word");
- document.write("10" + 3); 
+
+document.write(typeof "Word"); //declares typeof prints 'string'
+document.write("10" + 3); //attacthes a string to a number
+
+console.log(2 + 2); //prints results in console log
+console.log(6 > 9); 
+
+document.write(9 == 9); //checks if equal
+document.write(12 == 0);
+
+a = "12";
+b = 12; 
+c = "6";
+d = 6;
+e = 12;
+document.write(a === b); //checks if equal and have same typeof
+document.write(a == c);
+document.write(e === b);
+document.write(b === d);
+
+document.write(6 > 9 || 8 > 7)//or operator
+document.write(6 > 9 && 8 > 7)//and operator
+
+function not1() { //not operator function
+    document.getElementById("not").innerHTML = !(3 > 9);
+}
+
+
